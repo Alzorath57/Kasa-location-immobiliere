c0dc3f4ae6d73fd45b657664744daa6225e35113
Alzorath57 <julien.holtzinger@gmail.com>
2026-07-16 14:46:48 +0200

    feat: affichage des logements en grille centrée

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,21 @@
 import logements from "../data/logements.json";
 import Card from "../components/Card/Card";
+import "./Home.scss";
 
 function Home() {
   return (
     <>
       <h1>Home</h1>
-      {logements.map((logement) => (
-        <Card
-          key={logement.id}
-          title={logement.title}
-          cover={logement.cover}
-          description={logement.description}
-        />
-      ))}
+      <section className="cards-grid">
+        {logements.map((logement) => (
+          <Card
+            key={logement.id}
+            title={logement.title}
+            cover={logement.cover}
+            description={logement.description}
+          />
+        ))}
+      </section>
     </>
   );
 }
