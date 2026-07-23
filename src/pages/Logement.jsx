893c96262d1f768a74ceda6765b575dcf204e126
Alzorath57 <julien.holtzinger@gmail.com>
2026-07-23 23:05:27 +0200

    feat: récupération du logement selon l'id de l'URL

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,5 +1,14 @@
+import logements from "../data/logements.json";
+import { useParams } from "react-router-dom";
+import Error from "./Error";
+
 function Logement() {
-  return <h1>Logement </h1>;
+  const { id } = useParams();
+  const logement = logements.find((logement) => logement.id === id);
+  if (!logement) {
+    return <Error />;
+  }
+  return <h1>{logement.title}</h1>;
 }
 
 export default Logement;
