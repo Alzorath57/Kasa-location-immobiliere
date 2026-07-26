f7daa8fd942d65210c83cf438f3f6a84bfe21ba7
Alzorath57 <julien.holtzinger@gmail.com>
2026-07-26 12:21:12 +0200

    feat: ajout du carrousel dans logement avec fleche next previous

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,6 +1,9 @@
 import logements from "../data/logements.json";
 import { useParams } from "react-router-dom";
 import Error from "./Error";
+import { useState } from "react";
+import arrowLeft from "../assets/arrow-left.svg";
+import arrowRight from "../assets/arrow-right.svg";
 
 function Logement() {
   const { id } = useParams();
@@ -9,8 +12,29 @@ function Logement() {
     return <Error />;
   }
   const star = [1, 2, 3, 4, 5];
+  const [currentPictureIndex, setCurrentPictureIndex] = useState(0);
   return (
     <>
+      <img src={logement.pictures[currentPictureIndex]} alt={logement.title} />
+      <button
+        onClick={() =>
+          setCurrentPictureIndex(
+            (currentPictureIndex - 1 + logement.pictures.length) %
+              logement.pictures.length,
+          )
+        }
+      >
+        <img src={arrowLeft} alt="Photo précédente" />
+      </button>
+      <button
+        onClick={() =>
+          setCurrentPictureIndex(
+            (currentPictureIndex + 1) % logement.pictures.length,
+          )
+        }
+      >
+        <img src={arrowRight} alt="Photo suivante" />
+      </button>
       <h1>{logement.title}</h1>
       <p>{logement.location}</p>
       <ul>
