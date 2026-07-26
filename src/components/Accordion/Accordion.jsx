e1fbc5eef5aa1e3c2641514905442c167d814b9d
Alzorath57 <julien.holtzinger@gmail.com>
2026-07-26 17:59:12 +0200

    feat: ajout des fleche.svg et ajout dans accordion

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./Accordion.scss";
+import arrowUp from "../../assets/arrow-up.svg";
+import arrowDown from "../../assets/arrow-down.svg";
 
 function Accordion({ title, content }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,7 +14,13 @@ function Accordion({ title, content }) {
     <div className="accordion">
       <div className="accordion-header" onClick={toggleAccordion}>
         <h3>{title}</h3>
-        <span>{isOpen ? "-" : "+"}</span>
+        <span>
+          {isOpen ? (
+            <img src={arrowDown} alt="fleche vers le bas" />
+          ) : (
+            <img src={arrowUp} alt="fleche vers le haut" />
+          )}
+        </span>
       </div>
       {isOpen && <div className="accordion-content">{content}</div>}
     </div>
