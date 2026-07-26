66ed5f85c50817e3792842bc5939a6a81a9d7441
Alzorath57 <julien.holtzinger@gmail.com>
2026-07-26 18:37:23 +0200

    feat: ajout d'une conditions a Banner pour l'opacite de l'image

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ function Banner(props) {
   return (
     <div className="banner">
       <img src={props.cover} alt={props.description} />
-      <div className="overlay"></div>
+      {props.title && <div className="overlay"></div>}
       {props.title && <p>{props.title}</p>}
     </div>
   );
