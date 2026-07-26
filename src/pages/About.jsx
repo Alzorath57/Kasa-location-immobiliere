fda9841a34cdda1f828089ebf5c04dfcc741055e
Alzorath57 <julien.holtzinger@gmail.com>
2026-07-26 17:40:00 +0200

    feat: ajout du contenu de a propos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,32 @@
+import Banner from "../components/Banner/Banner";
+import bannerAbout from "../assets/banner-about.jpg";
+import Accordion from "../components/Accordion/Accordion";
+
 function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <Banner
+        cover={bannerAbout}
+        description="Paysage de montagnes et d'arbres"
+      />
+      <Accordion
+        title="Fiabilité"
+        content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+      ></Accordion>
+      <Accordion
+        title="Respect"
+        content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+      ></Accordion>
+      <Accordion
+        title="Service"
+        content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+      ></Accordion>
+      <Accordion
+        title="Sécurité"
+        content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+      ></Accordion>
+    </>
+  );
 }
 
 export default About;
